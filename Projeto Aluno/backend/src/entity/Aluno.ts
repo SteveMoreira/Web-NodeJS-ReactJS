980fe5d73ac67d72fb2e23b9941dc6a55e324b6f
SteveMoreira <stevemoreira2016@gmail.com>
2021-09-12 23:52:44 -0300

    segundo commit

---
+++ Projeto Aluno/backend/src/entity/Aluno.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn,
+    UpdateDateColumn } from 'typeorm';
+    @Entity()
+    export class Aluno{
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    title: string;
+    @Column()
+    description: string;
+    @Column({
+    default: false
+    })
+    finished: boolean;
+    @CreateDateColumn()
+    create_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+    }
